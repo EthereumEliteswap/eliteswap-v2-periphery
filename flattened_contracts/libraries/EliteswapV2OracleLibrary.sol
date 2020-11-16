06c25c4c55270f1bf67a9a3f0af3eac3943efdf8
EthereumEliteswap <73964644+EthereumEliteswap@users.noreply.github.com>
2020-11-16 16:45:41 +0200

    Update dependencies and init code hash

---
+++ flattened_contracts/libraries/EliteswapV2OracleLibrary.sol
@@ -0,0 +1,307 @@
+// File: @eliteswap/v2-core/contracts/interfaces/IEliteswapV2Pair.sol
+
+pragma solidity >=0.5.0;
+
+interface IEliteswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+// File: @eliteswap/lib/contracts/libraries/FullMath.sol
+
+// SPDX-License-Identifier: CC-BY-4.0
+pragma solidity >=0.4.0;
+
+// taken from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
+// license is CC-BY-4.0
+library FullMath {
+    function fullMul(uint256 x, uint256 y) internal pure returns (uint256 l, uint256 h) {
+        uint256 mm = mulmod(x, y, uint256(-1));
+        l = x * y;
+        h = mm - l;
+        if (mm < l) h -= 1;
+    }
+
+    function fullDiv(uint256 l, uint256 h, uint256 d) private pure returns (uint256) {
+        uint256 pow2 = d & -d;
+        d /= pow2;
+        l /= pow2;
+        l += h * ((-pow2) / pow2 + 1);
+        uint256 r = 1;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        r *= 2 - d * r;
+        return l * r;
+    }
+
+    function mulDiv(uint256 x, uint256 y, uint256 d, bool roundUp) internal pure returns (uint256) {
+        (uint256 l, uint256 h) = fullMul(x, y);
+        uint256 mm = mulmod(x, y, d);
+        if (mm > l) h -= 1;
+        l -= mm;
+        if (roundUp && mm > 0 && h < uint256(-1)) {
+            if (l > uint256(-1) - d) h += 1;
+            l += d;
+        }
+        require(h < d, 'FullMath: FULLDIV_OVERFLOW');
+        return fullDiv(l, h, d);
+    }
+
+    function mulDiv(uint256 x, uint256 y, uint256 d) internal pure returns (uint256) {
+        return mulDiv(x, y, d, false);
+    }
+}
+
+// File: @eliteswap/lib/contracts/libraries/Babylonian.sol
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.4.0;
+
+// computes square roots using the babylonian method
+// https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+library Babylonian {
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+        // else z = 0
+    }
+}
+
+// File: @eliteswap/lib/contracts/libraries/FixedPoint.sol
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.4.0;
+
+
+
+// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+library FixedPoint {
+    // range: [0, 2**112 - 1]
+    // resolution: 1 / 2**112
+    struct uq112x112 {
+        uint224 _x;
+    }
+
+    // range: [0, 2**144 - 1]
+    // resolution: 1 / 2**112
+    struct uq144x112 {
+        uint256 _x;
+    }
+
+    uint8 private constant RESOLUTION = 112;
+    uint256 private constant Q112 = 0x10000000000000000000000000000;
+    uint256 private constant Q224 = 0x100000000000000000000000000000000000000000000000000000000;
+    uint256 private constant LOWER_MASK = 0xffffffffffffffffffffffffffff; // decimal of UQ*x112 (lower 112 bits)
+
+    // encode a uint112 as a UQ112x112
+    function encode(uint112 x) internal pure returns (uq112x112 memory) {
+        return uq112x112(uint224(x) << RESOLUTION);
+    }
+
+    // encodes a uint144 as a UQ144x112
+    function encode144(uint144 x) internal pure returns (uq144x112 memory) {
+        return uq144x112(uint256(x) << RESOLUTION);
+    }
+
+    // decode a UQ112x112 into a uint112 by truncating after the radix point
+    function decode(uq112x112 memory self) internal pure returns (uint112) {
+        return uint112(self._x >> RESOLUTION);
+    }
+
+    // decode a UQ144x112 into a uint144 by truncating after the radix point
+    function decode144(uq144x112 memory self) internal pure returns (uint144) {
+        return uint144(self._x >> RESOLUTION);
+    }
+
+    // multiply a UQ112x112 by a uint, returning a UQ144x112
+    // reverts on overflow
+    function mul(uq112x112 memory self, uint256 y) internal pure returns (uq144x112 memory) {
+        uint256 z = 0;
+        require(y == 0 || (z = self._x * y) / y == self._x, 'FixedPoint: MUL_OVERFLOW');
+        return uq144x112(z);
+    }
+
+    // multiply a UQ112x112 by an int and decode, returning an int
+    // reverts on overflow
+    function muli(uq112x112 memory self, int256 y) internal pure returns (int256) {
+        uint256 z = FullMath.mulDiv(self._x, uint256(y < 0 ? -y : y), Q112);
+        require(z < 2**255, 'FixedPoint: MULI_OVERFLOW');
+        return y < 0 ? -int256(z) : int256(z);
+    }
+
+    // multiply a UQ112x112 by a UQ112x112, returning a UQ112x112
+    // lossy
+    function muluq(uq112x112 memory self, uq112x112 memory other) internal pure returns (uq112x112 memory) {
+        if (self._x == 0 || other._x == 0) {
+            return uq112x112(0);
+        }
+        uint112 upper_self = uint112(self._x >> RESOLUTION); // * 2^0
+        uint112 lower_self = uint112(self._x & LOWER_MASK); // * 2^-112
+        uint112 upper_other = uint112(other._x >> RESOLUTION); // * 2^0
+        uint112 lower_other = uint112(other._x & LOWER_MASK); // * 2^-112
+
+        // partial products
+        uint224 upper = uint224(upper_self) * upper_other; // * 2^0
+        uint224 lower = uint224(lower_self) * lower_other; // * 2^-224
+        uint224 uppers_lowero = uint224(upper_self) * lower_other; // * 2^-112
+        uint224 uppero_lowers = uint224(upper_other) * lower_self; // * 2^-112
+
+        // so the bit shift does not overflow
+        require(upper <= uint112(-1), 'FixedPoint: MULUQ_OVERFLOW_UPPER');
+
+        // this cannot exceed 256 bits, all values are 224 bits
+        uint256 sum = uint256(upper << RESOLUTION) + uppers_lowero + uppero_lowers + (lower >> RESOLUTION);
+
+        // so the cast does not overflow
+        require(sum <= uint224(-1), 'FixedPoint: MULUQ_OVERFLOW_SUM');
+
+        return uq112x112(uint224(sum));
+    }
+
+    // divide a UQ112x112 by a UQ112x112, returning a UQ112x112
+    function divuq(uq112x112 memory self, uq112x112 memory other) internal pure returns (uq112x112 memory) {
+        require(other._x > 0, 'FixedPoint: DIV_BY_ZERO_DIVUQ');
+        if (self._x == other._x) {
+            return uq112x112(uint224(Q112));
+        }
+        if (self._x <= uint144(-1)) {
+            uint256 value = (uint256(self._x) << RESOLUTION) / other._x;
+            require(value <= uint224(-1), 'FixedPoint: DIVUQ_OVERFLOW');
+            return uq112x112(uint224(value));
+        }
+
+        uint256 result = FullMath.mulDiv(Q112, self._x, other._x);
+        require(result <= uint224(-1), 'FixedPoint: DIVUQ_OVERFLOW');
+        return uq112x112(uint224(result));
+    }
+
+    // returns a UQ112x112 which represents the ratio of the numerator to the denominator
+    // lossy
+    function fraction(uint112 numerator, uint112 denominator) internal pure returns (uq112x112 memory) {
+        require(denominator > 0, 'FixedPoint: DIV_BY_ZERO_FRACTION');
+        return uq112x112((uint224(numerator) << RESOLUTION) / denominator);
+    }
+
+    // take the reciprocal of a UQ112x112
+    // reverts on overflow
+    // lossy
+    function reciprocal(uq112x112 memory self) internal pure returns (uq112x112 memory) {
+        require(self._x > 1, 'FixedPoint: DIV_BY_ZERO_RECIPROCAL_OR_OVERFLOW');
+        return uq112x112(uint224(Q224 / self._x));
+    }
+
+    // square root of a UQ112x112
+    // lossy between 0/1 and 40 bits
+    function sqrt(uq112x112 memory self) internal pure returns (uq112x112 memory) {
+        if (self._x <= uint144(-1)) {
+            return uq112x112(uint224(Babylonian.sqrt(uint256(self._x) << 112)));
+        }
+
+        uint8 safeShiftBits = 32;
+        while (safeShiftBits < 112) {
+            if (self._x < (uint256(1) << (256 - safeShiftBits - 2))) {
+                safeShiftBits += 2;
+            } else {
+                break;
+            }
+        }
+        return uq112x112(uint224(Babylonian.sqrt(uint256(self._x) << safeShiftBits) << ((112 - safeShiftBits) / 2)));
+    }
+}
+
+// File: contracts/libraries/EliteswapV2OracleLibrary.sol
+
+pragma solidity >=0.5.0;
+
+
+
+// library with helper methods for oracles that are concerned with computing average prices
+library EliteswapV2OracleLibrary {
+    using FixedPoint for *;
+
+    // helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
+    function currentBlockTimestamp() internal view returns (uint32) {
+        return uint32(block.timestamp % 2 ** 32);
+    }
+
+    // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
+    function currentCumulativePrices(
+        address pair
+    ) internal view returns (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) {
+        blockTimestamp = currentBlockTimestamp();
+        price0Cumulative = IEliteswapV2Pair(pair).price0CumulativeLast();
+        price1Cumulative = IEliteswapV2Pair(pair).price1CumulativeLast();
+
+        // if time has elapsed since the last update on the pair, mock the accumulated price values
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IEliteswapV2Pair(pair).getReserves();
+        if (blockTimestampLast != blockTimestamp) {
+            // subtraction overflow is desired
+            uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+            // addition overflow is desired
+            // counterfactual
+            price0Cumulative += uint(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
+            // counterfactual
+            price1Cumulative += uint(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
+        }
+    }
+}
---
+++ flattened_contracts/test/RouterEventEmitter.sol
@@ -0,0 +1,196 @@
+// File: contracts/interfaces/IEliteswapV2Router01.sol
+
+pragma solidity >=0.6.2;
+
+interface IEliteswapV2Router01 {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountToken, uint amountETH);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+}
+
+// File: contracts/test/RouterEventEmitter.sol
+
+pragma solidity =0.6.6;
+
+
+contract RouterEventEmitter {
+    event Amounts(uint[] amounts);
+
+    receive() external payable {}
+
+    function swapExactTokensForTokens(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapExactTokensForTokens.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactTokens(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapTokensForExactTokens.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactETHForTokens(
+        address router,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapExactETHForTokens.selector, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactETH(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapTokensForExactETH.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactTokensForETH(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapExactTokensForETH.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapETHForExactTokens(
+        address router,
+        uint amountOut,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IEliteswapV2Router01(router).swapETHForExactTokens.selector, amountOut, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+}
